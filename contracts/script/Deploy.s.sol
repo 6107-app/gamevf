1572c2e5329a3297f3e42ecfefa9d6dfe57b9846
nabbia-richman <nabbia00@gmail.com>
2026-05-23 01:08:44 +0800

    260523-fix：add deploy rod script-nara

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,9 +1,8 @@
-// 部署脚本
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "forge-std/Script.sol";
 import "../src/FishingGame.sol";
+import "../src/FishingRod.sol";
 import "./MockVRFLocal.sol";
 
 contract Deploy is Script {
@@ -11,14 +10,25 @@ contract Deploy is Script {
         vm.startBroadcast();
 
         MockVRFCoordinatorLocal vrf = new MockVRFCoordinatorLocal();
+
         FishingGame game = new FishingGame(
             address(vrf),
             bytes32(uint256(1)),
             1
         );
 
+        FishingRod rod = new FishingRod(
+            address(vrf),
+            bytes32(uint256(1)),
+            1
+        );
+
+        // 让 FishingGame 知道 FishingRod 的地址
+        game.setRodContract(address(rod));
+
         console.log("VRF:", address(vrf));
         console.log("FishingGame:", address(game));
+        console.log("FishingRod:", address(rod));
 
         vm.stopBroadcast();
     }
